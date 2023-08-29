41c6f19fc55cb76f2b70aa851835349d7afc0b85
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-08-29 15:12:12 +0800

    add initial cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 "use client";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 export default function Home() {
   const handleClick = () => {
     console.log("test");
   };
 
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button onClick={handleClick}>Test</Button>
+    <div className="flex items-center justify-center">
+      <Card className="w-60 mx-4">
+        <CardHeader className="text-center">Uptrend</CardHeader>
+        <CardContent className="text-center">120</CardContent>
+      </Card>
+      <Card className="w-60 mx-4">
+        <CardHeader className="text-center">Sideways</CardHeader>
+        <CardContent className="text-center">120</CardContent>
+      </Card>
+      <Card className="w-60 mx-4">
+        <CardHeader className="text-center">Downtrend</CardHeader>
+        <CardContent className="text-center">120</CardContent>
+      </Card>
     </div>
   );
 }
