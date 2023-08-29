8c0955a09a4e2f20d3ab535d5b159c2568c62955
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-08-29 16:25:48 +0800

    integrate go api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,36 @@
 "use client";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface ApiResponse {
+  message: string;
+}
+
+interface SetupsApi {
+  EMA4xMA8: string[];
+  FOB: any[];
+  FOD: any[];
+  _timeframe: string;
+  downtrend: string[];
+  sideways: string[];
+  timetaken: number;
+  uptrend: string[];
+}
+
 export default function Home() {
+  const [data, setData] = useState<ApiResponse | null>(null);
+  const [setupsData, setSetupsData] = useState<SetupsApi | null>(null);
+
+  useEffect(() => {
+    axios.get("http://holy-lake-1350.fly.dev").then((response) => {
+      setData(response.data);
+    });
+    axios.get("http://holy-lake-1350.fly.dev/setups/4h").then((response) => {
+      setSetupsData(response.data);
+    });
+  }, []);
+
   const handleClick = () => {
     console.log("test");
   };
@@ -9,15 +39,21 @@ export default function Home() {
     <div className="flex items-center justify-center">
       <Card className="w-60 mx-4">
         <CardHeader className="text-center">Uptrend</CardHeader>
-        <CardContent className="text-center">120</CardContent>
+        <CardContent className="text-center">
+          {setupsData?.uptrend.length}
+        </CardContent>
       </Card>
       <Card className="w-60 mx-4">
         <CardHeader className="text-center">Sideways</CardHeader>
-        <CardContent className="text-center">120</CardContent>
+        <CardContent className="text-center">
+          {setupsData?.sideways.length}
+        </CardContent>
       </Card>
       <Card className="w-60 mx-4">
         <CardHeader className="text-center">Downtrend</CardHeader>
-        <CardContent className="text-center">120</CardContent>
+        <CardContent className="text-center">
+          {setupsData?.downtrend.length}
+        </CardContent>
       </Card>
     </div>
   );
